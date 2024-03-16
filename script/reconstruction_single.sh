06e9915b8de3901573f255a272c2cd40f1f2d70e
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-16 21:18:43 +0900

    Update reconstruction_single.sh

--- script/reconstruction_single.sh
+++ script/reconstruction_single.sh
@@ -4,7 +4,7 @@ port_number=50054
 category="medical"
 obj_name="brain"
 benchmark="BraTS2020_Segmentation_multisegment"
-trigger_word='necrotic, ederma, tumor'
+trigger_word='brain'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
 file_name="2_binary_segmentation_BraTS2020"
@@ -16,7 +16,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --data_path "/home/dreamyou070/MyData/anomaly_detection/${category}/${obj_name}/${benchmark}/val" \
  --network_folder "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/models" \
  --obj_name "${obj_name}" \
- --prompt "n e t" \
+ --prompt "${trigger_word}" \
  --latent_res 64 \
  --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
