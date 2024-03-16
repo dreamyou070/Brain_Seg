98cd13f50c4742c6bdb23d56bc2d6daf305ba406
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-17 00:01:42 +0900

    train single

--- train_single.py
+++ train_single.py
@@ -291,7 +291,7 @@ if __name__ == "__main__":
     parser.add_argument("--vae_pretrained_dir", type=str)
     parser.add_argument("--local_hidden_states_globalize", action='store_true')
     parser.add_argument("--normal_activating_test", action='store_true')
-    parser.add_argument("--binary_test", action='store_true')
+    parser.add_argument("--train_single", action='store_true')
     parser.add_argument("--text_truncate", action='store_true')
     args = parser.parse_args()
     unet_passing_argument(args)
