4e388d59308b6d2302a15a369e8c0b601b9e8267
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-17 23:49:40 +0900

    Update evaluate.py

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -74,7 +74,7 @@ def calculate_IOU(segmentation_model, dataloader, device, text_encoder, unet, va
             mask_pred = mask_pred.permute(0, 2, 3, 1).detach().cpu().numpy() # 1,64,64,4
             mask_pred_argmax = np.argmax(mask_pred, axis=3) # 1,64,64
             # [2] real (1,4,64,64)
-            mask_true = mask_true.permute(0, 2, 3, 1).detach().numpy()
+            mask_true = mask_true.permute(0, 2, 3, 1).detach().cpu().numpy()
             # [3] IoU
             IOU_keras = MeanIoU(num_classes=4)
             IOU_keras.update_state(mask_pred_argmax, mask_true)
