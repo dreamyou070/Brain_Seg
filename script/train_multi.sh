1ff3c6681d2b2cd57943742895fd78a00c1ad22c
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-17 14:55:20 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -21,5 +21,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --trg_layer_list "['up_blocks_1_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
- --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
+ --do_attn_loss --do_cls_train \
  --resize_shape 512 --latent_res 64 --multiclassification_focal_loss --do_class_weight
