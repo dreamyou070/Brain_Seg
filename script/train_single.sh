c181e8aa4d53ab99d94227b3acde468e895a7f83
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-17 21:53:56 +0900

    Update train_single.sh

--- script/train_single.sh
+++ script/train_single.sh
@@ -1,17 +1,19 @@
 # !/bin/bash
 #
-port_number=50075
+port_number=50072
 category="medical"
 obj_name="brain"
 benchmark="BraTS2020_Segmentation_class_2"
 trigger_word='brain'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="10_class3_single_BraTS2020_text_truncate"
+file_name="10_class2_single_BraTS2020_text_truncate"
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
  --main_process_port $port_number ../train_single.py --log_with wandb \
  --output_dir "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}" \
+ --network_weights "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/models/epoch-000004.safetensors" \
+ --position_embedder_weights "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/position_embedder_4.safetensors" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 100 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "/home/dreamyou070/MyData/anomaly_detection/medical/brain/${benchmark}" \
