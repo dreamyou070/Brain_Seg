04db657577a155694393ea7a9aec31bd6187edc0
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-18 17:34:49 +0900

    Update train_single.sh

--- script/train_single.sh
+++ script/train_single.sh
@@ -3,13 +3,13 @@
 port_number=50071
 category="medical"
 obj_name="brain"
-benchmark="BraTS2020_Segmentation_class_1"
+benchmark="BraTS2020_Segmentation_class_3"
 trigger_word='brain'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="10_class1_single_BraTS2020_text_truncate"
+file_name="10_class3_single_BraTS2020_text_truncate"
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train_single.py --log_with wandb \
  --output_dir "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}" \
  --network_weights "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/models/epoch-000005.safetensors" \
