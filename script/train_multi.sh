9632c43e777c5807185734fc18e81e2724350c37
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-18 14:07:24 +0900

    Update train_multi.sh

--- script/train_multi.sh
+++ script/train_multi.sh
@@ -8,7 +8,7 @@ benchmark="BraTS2020_Segmentation_multisegment"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
 file_name="15_segmentation_model_multiclassification_focal_loss_change_diceloss_test_do_class_weight"
-#
+
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../segmentation_model.py --log_with wandb \
  --output_dir "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}" \
