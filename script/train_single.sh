ec7beaf79ec934aad13a632c513bd22d50224b43
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-18 17:58:12 +0900

    Update train_single.sh

--- script/train_single.sh
+++ script/train_single.sh
@@ -8,13 +8,13 @@ trigger_word='brain'
 layer_name='layer_3'
 sub_folder="up_16_32_64"
 file_name="10_class3_single_BraTS2020_text_truncate"
+#--network_weights "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/models/epoch-000005.safetensors" \
+ #--position_embedder_weights "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/position_embedder/position_embedder_5.safetensors" \
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_config \
  --main_process_port $port_number ../train_single.py --log_with wandb \
  --output_dir "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}" \
- --network_weights "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/models/epoch-000005.safetensors" \
- --position_embedder_weights "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}/position_embedder/position_embedder_5.safetensors" \
- --train_unet --train_text_encoder --start_epoch 4 --max_train_epochs 96 \
+ --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 30 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "/home/dreamyou070/MyData/anomaly_detection/medical/brain/${benchmark}" \
  --trigger_word "${trigger_word}" \
