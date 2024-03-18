c2d8ebfbdab3153ec28e31248a914e7c1c906462
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-18 21:13:18 +0900

    generate multi query

--- generate_multi_query.py
+++ generate_multi_query.py
@@ -19,13 +19,14 @@ def main() :
     base_folder = r'/home/dreamyou070/MyData/anomaly_detection/medical/brain'
     class_1_folder = os.path.join(base_folder, 'BraTS2020_Segmentation_class_1')
     class_2_folder = os.path.join(base_folder, 'BraTS2020_Segmentation_class_2')
-    class_3_folder = os.path.join(base_folder, 'BraTS2020_Segmentation_class_3')
-    class_t_folder = os.path.join(base_folder, 'BraTS2020_Segmentation_class_total')
+    #class_3_folder = os.path.join(base_folder, 'BraTS2020_Segmentation_class_3')
+    class_t_folder = os.path.join(base_folder, 'BraTS2020_Segmentation_class_1_2')
+    os.makedirs(class_t_folder, exist_ok = True)
     phases = os.listdir(class_1_folder)
     for phase in phases :
         c1_phase = os.path.join(class_1_folder, phase)
         c2_phase = os.path.join(class_2_folder, phase)
-        c3_phase = os.path.join(class_3_folder, phase)
+        #c3_phase = os.path.join(class_3_folder, phase)
         ct_phase = os.path.join(class_t_folder, phase)
         os.makedirs(ct_phase, exist_ok = True)
         normality_folders = os.listdir(c1_phase)
@@ -33,14 +34,14 @@ def main() :
         for normality_folder in normality_folders :
             c1_normality_folder = os.path.join(c1_phase, normality_folder)
             c2_normality_folder = os.path.join(c2_phase, normality_folder)
-            c3_normality_folder = os.path.join(c3_phase, normality_folder)
+            #c3_normality_folder = os.path.join(c3_phase, normality_folder)
             ct_normality_folder = os.path.join(ct_phase, normality_folder)
             os.makedirs(ct_normality_folder, exist_ok = True)
 
             for res in res_list :
                 c1_res_folder = os.path.join(c1_normality_folder, f'feature_{res}')
                 c2_res_folder = os.path.join(c2_normality_folder, f'feature_{res}')
-                c3_res_folder = os.path.join(c3_normality_folder, f'feature_{res}')
+                #c3_res_folder = os.path.join(c3_normality_folder, f'feature_{res}')
                 ct_res_folder = os.path.join(ct_normality_folder, f'feature_{res}')
                 os.makedirs(ct_res_folder, exist_ok = True)
                 if normality_folder == 'anormal' :
@@ -54,8 +55,8 @@ def main() :
                     c1_feature = c1_feature.permute(0,2,3,1) # head, res,res,dim
                     c2_feature = torch.load(os.path.join(c2_res_folder, feature))
                     c2_feature = c2_feature.permute(0, 2, 3, 1)
-                    c3_feature = torch.load(os.path.join(c3_res_folder, feature))
-                    c3_feature = c3_feature.permute(0, 2, 3, 1)
+                    #c3_feature = torch.load(os.path.join(c3_res_folder, feature))
+                    #c3_feature = c3_feature.permute(0, 2, 3, 1)
 
                     if normality_folder == 'anormal' :
                         mask_arr = os.path.join(ct_mask_folder, f'{name}.npy')
@@ -63,18 +64,21 @@ def main() :
                         c0_mask = np.where(mask == 0, 1, 0)
                         c1_mask = np.where(mask == 1, 1, 0)
                         c2_mask = np.where(mask == 2, 1, 0)
-                        c3_mask = np.where(mask == 3, 1, 0)
+                        #c3_mask = np.where(mask == 3, 1, 0)
                         c0_mask = expand_mask(c0_mask, head, dim)
                         c1_mask = expand_mask(c1_mask, head, dim)
                         c2_mask = expand_mask(c2_mask, head, dim)
-                        c3_mask = expand_mask(c3_mask, head, dim)
+                        #c3_mask = expand_mask(c3_mask, head, dim)
 
                         # [4] total feature
-                        total_feature = c1_feature * c1_mask + c2_feature * c2_mask + c3_feature * c3_mask + (c1_feature+c2_feature+c3_feature)/3 * c0_mask
+                        #total_feature = c1_feature * c1_mask + c2_feature * c2_mask + c3_feature * c3_mask + (c1_feature+c2_feature+c3_feature)/3 * c0_mask
+                        total_feature = c1_feature * c1_mask + c2_feature * c2_mask + (
+                                    c1_feature + c2_feature) / 2 * c0_mask
                         total_feature = total_feature.permute(0,3,1,2)
                         torch.save(total_feature, os.path.join(ct_res_folder, feature))
                     else :
-                        total_feature = (c1_feature + c2_feature + c3_feature)/3
+                        #total_feature = (c1_feature + c2_feature + c3_feature)/3
+                        total_feature = (c1_feature + c2_feature) / 2
                         total_feature = total_feature.permute(0, 3, 1, 2)
                         torch.save(total_feature, os.path.join(ct_res_folder, feature))
 
