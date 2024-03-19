77aa82a096aa6f158b8ac04b8b206c2b6525edea
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-20 00:38:48 +0900

    aq

--- segmentation_head.py
+++ segmentation_head.py
@@ -175,24 +175,29 @@ def main(args):
             masks_pred = segmentation_head(x16_out, x32_out, x64_out) # 1,4,128,128
 
             # [5.1] Multiclassification Loss
-            """
+
             loss = criterion(masks_pred, gt)
+            print(f'loss1 = {loss.shape}')
             loss_dict['cross_entropy_loss'] = loss.item()
-            """
+
             # [5.2] Focal Loss
             masks_pred_ = masks_pred.permute(0, 2, 3, 1) # 1,128,128,4
             masks_pred_ = masks_pred_.view(-1, masks_pred_.shape[-1])
             focal_loss = loss_multi_focal(masks_pred_,  # N,C
                                           gt_flat.squeeze().to(masks_pred.device))  # N
-            loss = focal_loss
+            print(f'loss2 = {focal_loss.shape}')
+            loss += focal_loss
             loss_dict['focal_loss'] = focal_loss.item()
             # [5.3] Dice Loss
             y = gt_flat.view(128,128).unsqueeze(dim=0)
             if args.use_dice_anomal_loss:
-                loss += dice_loss_anomal(y_pred=masks_pred, y_true=y.unsqueeze(0).to(torch.int64))
+                dice_loss = dice_loss_anomal(y_pred=masks_pred, y_true=y.unsqueeze(0).to(torch.int64))
+                loss += dice_loss
             else:
-                loss += dice_loss_fn(y_pred=masks_pred, y_true=y.unsqueeze(0).to(torch.int64))
-            
+                dice_loss = dice_loss_fn(y_pred=masks_pred, y_true=y.unsqueeze(0).to(torch.int64))
+                loss += dice_loss
+            print(f'loss3 = {dice_loss.shape}')
+
             loss = loss.to(weight_dtype)
             current_loss = loss.detach().item()
             if epoch == args.start_epoch:
