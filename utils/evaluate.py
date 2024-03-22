7206809973276d5c66d532b6678a1214d77d9d47
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-22 10:48:25 +0900

    Update evaluate.py

--- utils/evaluate.py
+++ utils/evaluate.py
@@ -65,48 +65,60 @@ def evaluation_check(segmentation_head, dataloader, device, text_encoder, unet, 
         y_true_list, y_pred_list = [], []
         dice_coeff_list = []
 
-        #for batch in tqdm(dataloader, total=num_val_batches, desc='Validation round', unit='batch', leave=False):
         for global_num, batch in enumerate(dataloader):
-            if global_num < 100 :
-                encoder_hidden_states = text_encoder(batch["input_ids"].to(device))["last_hidden_state"]
-                if args.text_truncate:
-                    encoder_hidden_states = encoder_hidden_states[:, :2, :]
-                image = batch['image'].to(dtype=weight_dtype)  # 1,3,512,512
-                gt = batch['gt'].to(dtype=weight_dtype)  # 1,4,128,128
-                gt_flat = batch['gt_flat'].to(dtype=weight_dtype)  # 1,128*128
-                with torch.no_grad():
-                    latents = vae.encode(image).latent_dist.sample() * args.vae_scale_factor
-                with torch.set_grad_enabled(True):
-                    unet(latents, 0, encoder_hidden_states, trg_layer_list=args.trg_layer_list,
-                         noise_type=position_embedder)
-                query_dict, key_dict, attn_dict = controller.query_dict, controller.key_dict, controller.attn_dict
-                controller.reset()
-                q_dict = {}
-                for layer in args.trg_layer_list:
-                    query = query_dict[layer][0].squeeze()  # head, pix_num, dim
-                    # resizing
-                    res = int(query.shape[1] ** 0.5)
-                    q_dict[res] = reshape_batch_dim_to_heads(query)
-                x16_out, x32_out, x64_out = q_dict[16], q_dict[32], q_dict[64]
-                # x16_out, x32_out, x64_out = [1,dim,res,res]
-                masks_pred = segmentation_head(x16_out, x32_out, x64_out)  # 1,4,128,128
-                #######################################################################################################################
-                # segmentation model
-                # [1] pred
-                mask_pred_ = masks_pred.permute(0, 2, 3, 1).detach().cpu().numpy()  # 1,128,128,4
-                mask_pred_argmax = np.argmax(mask_pred_, axis=3).flatten()          # 128*128
-                y_pred_list.append(torch.Tensor(mask_pred_argmax))
+            encoder_hidden_states = text_encoder(batch["input_ids"].to(device))["last_hidden_state"]
+            if args.text_truncate :
+                encoder_hidden_states = encoder_hidden_states[:,:2,:]
+            # ------------------------------------------------------------------------------------------------------------
+            image = batch['image'].to(dtype=weight_dtype)                                   # 1,3,512,512
+            gt = batch['gt'].to(dtype=weight_dtype)                                         # 1,4,128,128
+            gt_flat = batch['gt_flat'].to(dtype=weight_dtype)                               # 1,128*128
+            with torch.no_grad():
+                latents = vae.encode(image).latent_dist.sample() * args.vae_scale_factor
+            with torch.set_grad_enabled(True):
+                unet(latents, 0, encoder_hidden_states, trg_layer_list=args.trg_layer_list, noise_type=position_embedder)
+            query_dict, key_dict, attn_dict = controller.query_dict, controller.key_dict, controller.attn_dict
+            controller.reset()
+            q_dict = {}
+            k_dict = {}
 
-                mask_true = gt_flat.detach().cpu().numpy().flatten()                # 128*128
-                y_true_list.append(torch.Tensor(mask_true))
+            def reshape_batch_dim_to_heads(tensor):
+                batch_size, seq_len, dim = tensor.shape
+                head_size = 8
+                tensor = tensor.reshape(batch_size // head_size, head_size, seq_len, dim) # 1,8,pix_num, dim -> 1,pix_nun, 8,dim
+                tensor = tensor.permute(0, 2, 1, 3).contiguous().reshape(batch_size // head_size, seq_len, dim * head_size) # 1, pix_num, long_dim
+                res = int(seq_len ** 0.5)
+                tensor = tensor.view(batch_size // head_size, res,res, dim * head_size).contiguous()
+                tensor = tensor.permute(0,3,1,2).contiguous()  # 1, dim, res,res
+                return tensor
 
-                # [2] dice coefficient
-                from utils.dice_score import dice_loss
-                dice_coeff = 1-dice_loss(F.softmax(masks_pred, dim=1).float(),  # class 0 ~ 4 check best
-                                          gt,multiclass=True)
-                dice_coeff_list.append(dice_coeff.detach().cpu())
+            for layer in args.trg_layer_list:
+                query = query_dict[layer][0].squeeze()  # head, pix_num, dim
+                res = int(query.shape[1] ** 0.5)
+                q_dict[res] = reshape_batch_dim_to_heads(query) # 1, res,res,dim
+                k_dict[res] = key_dict[layer][0]
+            x16_out, x32_out, x64_out = q_dict[16], q_dict[32], q_dict[64]
+            key64 = k_dict[64]
+            # x16_out, x32_out, x64_out = [1,dim,res,res]
+            if not args.cross_attn_base :
+                masks_pred = segmentation_head(x16_out, x32_out, x64_out) # 1,4,128,128
             else :
-                break
+                masks_pred = segmentation_head(x16_out, x32_out, x64_out, key64)  # 1,4,128,128
+            #######################################################################################################################
+            # segmentation model
+            # [1] pred
+            mask_pred_ = masks_pred.permute(0, 2, 3, 1).detach().cpu().numpy()  # 1,128,128,4
+            mask_pred_argmax = np.argmax(mask_pred_, axis=3).flatten()          # 128*128
+            y_pred_list.append(torch.Tensor(mask_pred_argmax))
+
+            mask_true = gt_flat.detach().cpu().numpy().flatten()                # 128*128
+            y_true_list.append(torch.Tensor(mask_true))
+
+            # [2] dice coefficient
+            from utils.dice_score import dice_loss
+            dice_coeff = 1-dice_loss(F.softmax(masks_pred, dim=1).float(),  # class 0 ~ 4 check best
+                                      gt,multiclass=True)
+            dice_coeff_list.append(dice_coeff.detach().cpu())
         y_hat = torch.cat(y_pred_list)
         y = torch.cat(y_true_list)
         score = confusion_matrix(y, y_hat)
