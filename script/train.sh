fd5d723524b4b4365ed2e7cfc52964d06ee01e19
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-23 19:48:49 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -1,13 +1,14 @@
 # !/bin/bash
 #
-port_number=58802
+port_number=58801
 category="medical"
 obj_name="leader_polyp"
-benchmark="bkai-igh-neopolyp"
+benchmark="bkai-igh-neopolyp_sy"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="6_segmentation_model_b_crossentropy"
+file_name="8_segmentation_model_a_crossentropy_data_sy"
 #--use_position_embedder \
+#--aggregation_model_b
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
  --output_dir "../../result/${category}/${obj_name}/${layer_name}/${sub_folder}/${file_name}" \
@@ -22,5 +23,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --n_classes 3 \
- --mask_res 256 \
- --aggregation_model_b
+ --mask_res 256
