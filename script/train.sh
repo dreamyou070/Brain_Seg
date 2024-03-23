ae7b0f27d81fb0fdaf4a6eb34ff461bad4190548
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-23 15:01:22 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -1,12 +1,12 @@
 # !/bin/bash
 #
-port_number=50641
+port_number=50642
 category="medical"
 obj_name="leader_polyp"
 benchmark="bkai-igh-neopolyp"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="2_segmentation_model_a_focal_loss_dice_loss"
+file_name="2_segmentation_model_b_focal_loss_dice_loss"
 #--use_position_embedder \
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
@@ -23,4 +23,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --n_classes 3 \
  --mask_res 256 \
+ --aggregation_model_b \
  --use_focal_loss
