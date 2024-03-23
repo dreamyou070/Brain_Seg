a7bde741fd461906408507dc03c0a394ff124a65
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-23 22:51:24 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -1,12 +1,12 @@
 # !/bin/bash
 #
-port_number=58891
+port_number=58892
 category="medical"
 obj_name="leader_polyp"
 benchmark="bkai-igh-neopolyp_sy"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="10_segmentation_model_a_crossentropy_focall_loss_data_sy_layer_norm"
+file_name="10_segmentation_model_b_crossentropy_focall_loss_data_sy_layer_norm"
 #--use_position_embedder \
 #--aggregation_model_b
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
@@ -23,4 +23,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
                     'up_blocks_2_attentions_2_transformer_blocks_0_attn2',
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --n_classes 3 \
- --mask_res 256 --cross_entropy_focal_loss_both
+ --mask_res 256 --cross_entropy_focal_loss_both \
+ --aggregation_model_b
