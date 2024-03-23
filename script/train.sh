edc365b1a9bba447d608bb363333e734e6ed299a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-23 11:56:09 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -1,12 +1,12 @@
 # !/bin/bash
 #
-port_number=50641
+port_number=50642
 category="medical"
 obj_name="leader_polyp"
 benchmark="bkai-igh-neopolyp"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="1_segmentation_model_a"
+file_name="1_segmentation_model_b"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
@@ -25,4 +25,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --use_position_embedder \
  --n_classes 3 \
  --do_attn_loss \
- --mask_res 256
+ --mask_res 256 \
+ --aggregation_model_b
