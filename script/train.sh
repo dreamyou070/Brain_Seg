21404961175a6bbf964d67e48bfac9c5cbd3be70
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-24 22:05:32 +0900

    train 8th

--- script/train.sh
+++ script/train.sh
@@ -1,12 +1,12 @@
 # !/bin/bash
 #
-port_number=58809
+port_number=58808
 category="medical"
 obj_name="leader_polyp"
 benchmark="bkai-igh-neopolyp_sy"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="9_segmentation_model_a_layer_norm_silu_crossentropy_focal_loss_do_attn_loss_do_dice_loss"
+file_name="8_segmentation_model_a_layer_norm_leaky_relu_crossentropy_do_attn_loss"
 # #--do_attn_loss
 #--aggregation_model_b
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
@@ -29,6 +29,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
                     'up_blocks_3_attentions_2_transformer_blocks_0_attn2',]" \
  --norm_type "layer_norm" \
  --nonlinearity_type "silu" \
- --cross_entropy_focal_loss_both \
- --do_attn_loss \
- --do_dice_loss
+ --do_attn_loss
