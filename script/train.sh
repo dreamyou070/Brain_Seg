db319c96460bcbebc130ec9a5e5c8d38acef4d96
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-03-24 20:23:05 +0900

    train4

--- script/train.sh
+++ script/train.sh
@@ -1,13 +1,13 @@
 # !/bin/bash
 #
-port_number=58808
+port_number=58804
 category="medical"
 obj_name="leader_polyp"
 benchmark="bkai-igh-neopolyp_sy"
 layer_name='layer_3'
 sub_folder="up_16_32_64"
-file_name="8_segmentation_model_a_instance_norm_leaky_relu_crossentropy_focal_loss_do_attn_loss"
-# \
+file_name="4_segmentation_model_a_layer_norm_leaky_relu_crossentropy_focal_loss"
+# #--do_attn_loss
 #--aggregation_model_b
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
@@ -26,7 +26,6 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --n_classes 3 \
  --mask_res 256 \
  --cross_entropy_focal_loss_both \
- --norm_type "instance_norm" \
+ --norm_type "layer_norm" \
  --nonlinearity_type "leaky_relu" \
- --cross_entropy_focal_loss_both \
- --do_attn_loss
+ --cross_entropy_focal_loss_both
